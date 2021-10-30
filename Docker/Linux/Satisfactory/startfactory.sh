10440409991c4e26afff8af9570ffbd11f87cf9b
thelamescriptkiddiemax <m6ximilian+git46584654@gmail.com>
2021-10-30 21:48:52 +0200

    stable

--- Docker/Linux/Satisfactory/startfactory.sh
+++ Docker/Linux/Satisfactory/startfactory.sh
@@ -4,11 +4,4 @@ set -e
 
 steamcmd +login anonymous +force_install_dir /home/steam/server +app_update 1690800 +quit
 
-mkdir -p /home/steam/server/FactoryGame/Saved
-mkdir -p /home/steam/server/FactoryGame/Saved/config
-mkdir -p /home/steam/server/FactoryGame/Saved/SaveGames
-mkdir -p /home/steam/server/FactoryGame/Saved/SaveGames/server
-
-cp /home/steam/Engine.ini /home/steam/server/FactoryGame/Saved/config/Engine.ini
-
 /home/steam/server/FactoryServer.sh
