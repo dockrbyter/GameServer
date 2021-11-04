dce4fd6c5064b5d95837b0a8380d98abfef92856
thelamescriptkiddiemax <m6ximilian+git46584654@gmail.com>
2021-11-04 18:37:45 +0100

    fix for fix

--- Docker/Linux/Satisfactory/startfactory.sh
+++ Docker/Linux/Satisfactory/startfactory.sh
@@ -4,4 +4,4 @@ set -e
 
 steamcmd +login anonymous +force_install_dir /home/steam/server +app_update 1690800 +quit
 
-/home/steam/server/FactoryServer.sh
+/home/steam/server/FactoryServer.sh -multihome=0.0.0.0
