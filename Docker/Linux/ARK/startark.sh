b34ad00c4c5e67a6313e162acb0bb94ba903426a
Geekmeleon <12664235+Geekmeleon@users.noreply.github.com>
2022-05-06 11:54:18 -0500

    Update startark.sh
    Change chown to a user variable to allow custom usernames

--- Docker/Linux/ARK/startark.sh
+++ Docker/Linux/ARK/startark.sh
@@ -2,7 +2,7 @@
 
 set -e
 steamcmd +login anonymous +force_install_dir /home/steam/server +app_update $GAMEID +quit
-chown -R "steam:steam" "/home/steam/server"
+chown -R "$USER:$USER" "/home/steam/server"
 
 ulimit -n 100000
 ./server/ShooterGame/Binaries/Linux/ShooterGameServer $MAP?listen?Multihome=$MULTIHOMEIP?SessionName=$SERVERNAME?MaxPlayers=$MAXPLAYERS?ServerAdminPassword=$ADMINPW?Port=$PORTGAME?QueryPort=$PORTQUERY?RCONEnabled=$RCONON?RCONPort=$PORTRCON?PreventDownloadSurvivors=False?PreventDownloadItems=False?PreventDownloadDinos=False?PreventUploadSurvivors=False?PreventUploadItems=False?PreventUploadDinos=False?noTributeDownloads=False -ClusterDirOverride=$DIRCLUSTER -clusterid=$CLUSTERID -nosteamclient -ActiveEvent=WinterWonderland -server -log
